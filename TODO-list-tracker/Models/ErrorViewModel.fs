bc858b2499d094defbfc209b94e3c718d3fba07b
cbt <ctu11@my.centennialcollege.ca>
2025-11-20 02:30:38 -0500

    Rebuilt project
    -added sln file
    -retained readme

---
+++ TODO-list-tracker/Models/ErrorViewModel.fs
@@ -0,0 +1,9 @@
+namespace TODO_list_tracker.Models
+
+open System
+
+type ErrorViewModel =
+    { RequestId: string }
+
+    member this.ShowRequestId =
+        not (String.IsNullOrEmpty(this.RequestId))
