4884005ebb115abe51d99eab5c98decb1256f025
Fatima Elmi <ElmiFatima2025@gmail.com>
2025-11-21 12:21:22 -0500

    Add TaskItem and Category models and migration

---
+++ TODO-list-tracker/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TODO_list_tracker.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public string UserId { get; set; }
+
+        public ICollection<TaskItem> Tasks { get; set; }
+    }
+}
